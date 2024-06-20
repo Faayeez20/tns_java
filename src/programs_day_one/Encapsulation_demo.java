0638381021986f28ed3abf887924416dcb4859b0
Faayeez20 <faayeez.211447.it@mhssce.ac.in>
2024-06-20 13:37:27 +0530

    first commi

---
+++ src/programs_day_one/Encapsulation_demo.java
@@ -0,0 +1,18 @@
+package programs_day_one;
+
+public class Encapsulation_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Encapsulation ob = new Encapsulation();
+		
+		ob.setSerialnum(100);
+		ob.setName("John");
+		ob.setAge(20);
+		
+		System.out.println(ob);
+
+	}
+
+}
